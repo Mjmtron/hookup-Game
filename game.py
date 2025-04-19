28b711c33c55cf13f09b2b29628eec1e89197542
Mjmtron <95286009+Mjmtron@users.noreply.github.com>
2025-04-19 08:18:39 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,36 +1,14 @@
-print(
-        'WELCOME TO HOOK UP GAME'
-        )
-print(
-        'STORY'
-        )
-print(
-        'IN THIS GAME YOU HAVE TO REACH TO A GIVEN LOCATION BUT REMEMBER THAT IS'
-        )
-print(
-        'NOT SO EASY THERE WILL BE OPSTICAL SPAWNING EVERY SECOND YOU HAVE TO'
-        )
-print(
-        'AVOID THEM AND REACH TO THE RED SPOT IF YOU WANT TO WIN THIS GAME.'
-        )
-print(
-        'IF YOU WIN YOU GET TO HERE HOOK UP SONG AND IF YOU LOOSE YOU GET TO HERE '
-        )
-print(
-        'dhinchak poojas song'
-        )
-print(
-        '!ALL THE BEST FOR GAME PLAY!'
-        )
-print(
-        'NOTE'
-      )
-print(
-        'GAME WILL NOT WORK WITHOUT ENTERING NAME'
-        )
-name=input(
-        'ENTER YOUR NAME:'
-           )
+print('WELCOME TO HOOK UP GAME')
+print('STORY')
+print('IN THIS GAME YOU HAVE TO REACH TO A GIVEN LOCATION BUT REMEMBER THAT IS')
+print('NOT SO EASY THERE WILL BE OPSTICAL SPAWNING EVERY SECOND YOU HAVE TO')
+print('AVOID THEM AND REACH TO THE RED SPOT IF YOU WANT TO WIN THIS GAME.')
+print('IF YOU WIN YOU GET TO HERE HOOK UP SONG AND IF YOU LOOSE YOU GET TO HERE ')
+print('dhinchak poojas song')
+print('!ALL THE BEST FOR GAME PLAY!')
+print('NOTE')
+print('GAME WILL NOT WORK WITHOUT ENTERING NAME')
+name=input('ENTER YOUR NAME:')
 
 import pygame
 import random as R
@@ -42,43 +20,15 @@ pygame.init(
         )
 #
 #colour code
-w=(
-        255
-        ,255,
-        255)
-r=(
-        255
-        ,0
-        ,0
-        )
-g=(
-        0,
-        255,
-        0)
-b=(
-        0,
-        0,
-        255
-        )
-y=(
-        255,
-        255,
-        0
-        )
-B=(
-        0,
-        0,
-        0
-        )
+w=(255,255,255)
+r=(255,0,0)
+g=(0,255,0)
+b=(0,0,255)
+y=(255,255,0)
+B=(0,0,0)
 #
  #screen
-screen=pygame.display.set_mode(
-        (
-                600
-                ,
-                300
-                                )
-                               ) #size of screen
+screen=pygame.display.set_mode((600,300),pygame.RESIZABLE) #size of screen
 pygame.display.set_caption(
         'DODGER GAME '
                            ) #name of game
@@ -95,89 +45,30 @@ font=pygame.font.SysFont(
         ,
         55
                          )
-def text(
-        t
-        ,color
-        ,x
-        ,y
-        ):
-        tex=font.render(
-                t
-                ,
-                True
-                ,
-                color
-                        )
-        screen.blit(
-                tex,[
-                        x
-                     ,
-                        y
-                     ]
-                    )
-def welcome(
-        ):
-    WEI=pygame.image.load(
-            'WI.jpg'
-            )
-    WEI=pygame.transform.scale(
-            WEI,
-            (
-                    300
-             ,200
-             )
-            ).convert_alpha(
-                    )
+def text(t,color,x,y):
+        tex=font.render(t,True,color)
+        screen.blit(tex,[x,y])
+def welcome():
+    WEI=pygame.image.load('WI.jpg')
+    WEI=pygame.transform.scale(WEI,(300,200)).convert_alpha()
     # WELCOME BGM
     #bi=pygame.image.load('mj.jpg') # you can add image on wlcome screen 
     
-    pygame.mixer.music.load(
-            'wbg.mp3'
-            )
-    pygame.mixer.music.play(
-            )
+    pygame.mixer.music.load('wbg.mp3')
+    pygame.mixer.music.play()
     _exit_=False
 
     # WRITING TXT ON WLCOME PAGE
     
     while not _exit_:
         screen.fill(
-                (
-                        233,
-                        210
-                        ,229
-                        )
-                )
-        text(
-                'DODGER '
-                ,r
-                ,300
-                ,50
-                )
-        text(
-                'GAME',
-                r,
-                400,
-                100
-                )
-        text(
-                'PRESS SPACE TO START'
-                ,
-                B
-                ,100
-                ,225
-                )
-        screen.blit(
-                WEI
-                ,(
-                        0
-                        ,
-                        0
-                        )
-                )
+                (233,210,229))
+        text('DODGER ',r,300,50)
+        text('GAME',r,400,100)
+        text('PRESS SPACE TO START',B,100,225)
+        screen.blit(WEI,(0,0))
         
-        for event in pygame.event.get(
-                ):
+        for event in pygame.event.get():
             if event.type==pygame.QUIT:
                     _exit_=True
         
@@ -194,10 +85,8 @@ def welcome(
 
 ########################################################################################################################################################################3###
 #YOUR GAME CODE / GAME LOOP
-def gameloop(
-        ):
-    start_time=t.time(
-            )        
+def gameloop():
+    start_time=t.time()        
     #specific variable
     _exit_=False
 
@@ -216,34 +105,17 @@ def gameloop(
     w1x=160
     w1y=160
 
-    if(
-            not os.path.exists(
-            'name.txt'
-
-                          )
-       ):
-            with open(
-                    'name.txt'
-                    ,'w'
-                    )as f:
-                    f.write(
-                            str(name)
-                            )
-    with open(
-            'highscore.txt'
-            ,'r'
-            )as f:
-             hiscore=f.read(
-                     )
+    if(not os.path.exists('name.txt')):
+            with open('name.txt','w')as f:
+                    f.write(str(name))
+    with open('highscore.txt','r')as f:
+         hiscore=f.read()
 
 
 ########################################################################################################################################################################
     # GAME BGM
-    pygame.mixer.music.load(
-            'gbg.mp3'
-                            )
-    pygame.mixer.music.play(
-            )
+    pygame.mixer.music.load('gbg.mp3')
+    pygame.mixer.music.play( )
     _exit_=False
 
 
@@ -251,37 +123,14 @@ def gameloop(
     while not _exit_:
         # IF YOU LOOSE
         if game_over:
-            lo=pygame.image.load(
-                    "los.jpg"
-                    )
-            lo=pygame.transform.scale(
-                    lo,
-                                      (
-                    300,
-                    300
-                                          )
-                                      ).convert_alpha(
-                                              )
+            lo=pygame.image.load( "los.jpg")
+            lo=pygame.transform.scale(lo,(391,234)).convert_alpha()
 
-            screen.fill(
-                    w
-                    )
-            screen.blit(
-                    lo,(
-                            100,
-                            0
-                        )
-                    )
+            screen.fill(w)
+            screen.blit( lo,(100,0))
 
-            text(
-                    'PRESS SPACE',b,320,230
-                 )
-            text(
-                    'FOR MENU',
-                    b,
-                    400,
-                    260
-                 )#('TEXT',COLOUR,LOCATION ON X,LOCATION ON Y)
+            text('PRESS SPACE',b,320,230)
+            text('FOR MENU',b,400,260)#('TEXT',COLOUR,LOCATION ON X,LOCATION ON Y)
            
             for event in pygame.event.get():
                     if event.type==pygame.QUIT:
@@ -292,54 +141,24 @@ def gameloop(
                                        )
         # IF YOU WIN
         elif win:
-             Wi=pygame.image.load(
-                     "win.jpg"
-                                  )
+             Wi=pygame.image.load("win.jpg")
 
-             Wi=pygame.transform.scale(
+             Wi=pygame.transform.scale(Wi,(300,300)).convert_alpha()
 
-                     Wi,
-                                       (
-                     300,300
-                                           )
-                                       ).convert_alpha(
-                                               )
-
-             screen.fill(
-                     w
-                         )
-             screen.blit(
-                     Wi,
-                         (
-                     100,
-                             0
-                             )
-                         )
-             text(
-                     'PRESS SPACE',
-                     b,
-                     320,
-                     230
-                  )
-             text(
-                     'FOR MENU',
-                     b,
-                     400,
-                     260
-                  )
+             screen.fill(w)
+             screen.blit(Wi,(100,0))
+             text('PRESS SPACE', b,320,230)
+             text('FOR MENU',b,400,260)
          
-             for event in pygame.event.get(
-                     ):
+             for event in pygame.event.get():
                     if event.type==pygame.QUIT:
                             _exit_=True
                     if event.type==pygame.KEYDOWN:
                         if event.key==pygame.K_SPACE:
-                               welcome(
-                                       )
+                               welcome()
       
         elif _exit_:
-                quit(
-                        )
+                quit()
         else:
         #player control
             for event in pygame.event.get():
@@ -367,137 +186,52 @@ def gameloop(
 
     ############################################################################################################
 
-            W=pygame.image.load(
-                    "BG.PNG"
-                                )
-            W=pygame.transform.scale(W,
-                                     (600,300
-                                        )
-                                     ).convert_alpha()
+            W=pygame.image.load("BG.PNG")
+            W=pygame.transform.scale(W,(600,300)).convert_alpha()
             
-            screen.fill(g
-                        )     #colour of screen
-            screen.blit(
-                    W,
-                    (
-                            0
-                            ,0
-                            )
-                        )
+            screen.fill(g)     #colour of screen
+            screen.blit(W,(0,0))
                        
             #screen.blit(bi,(0,0))            #projecting image
                                       #postion , size
-            pygame.draw.rect(
-                    screen
-                    ,b
-                    ,[
-                            px,
-                            py,
-                            10,
-                            10
-                            ]
-                             )
+            pygame.draw.rect(screen,b,[px, py,10, 10])
                                         
             
             #wall
             
               
-            W=pygame.draw.rect(
-                    screen,
-                    r,
-                    [
-                            500
-                            ,200
-                            ,10
-                            ,10
-                            ]
-                               )
+            W=pygame.draw.rect(screen,r,[500,200,10,10])
             
             if px==500 and py==200:
                 win=True
-                pygame.mixer.music.load(
-                        'hookup.mp3'
-                                        )
-                pygame.mixer.music.play(
-                        )
+                pygame.mixer.music.load('hookup.mp3')
+                pygame.mixer.music.play()
 
-            for i in range(
-                    0
-                    ,20
-                           ):#you can increase or decrase opstical by changeing (0,xyz)
+            for i in range(0,20):#you can increase or decrase opstical by changeing (0,xyz)
 #                                                                                      ^
-                pygame.draw.rect(
-                        screen
-                        ,
-                        B
-                        ,
-                        [R.randint(
-                                0
-                                ,
-                                600
-                                ),
-                         R.randint(
-                                 0,300
-                                 ),20,10
-                         ]
-                        )
-                if abs(
-                        px-R.randint(
-                                0
-                                ,
-                                600
-                                )
-                        )<6 and abs(
-                                py-R.randint(
-                        0
-                        ,
-                        600
-                                                                   )
-                                                      )<6:
+                pygame.draw.rect(screen,B,[R.randint(0,600),R.randint(0,300),20,10])
+                if abs(px-R.randint(0,600))<6 and abs(py-R.randint(0,600))<6:
                     px=px-10
                     game_over=True
-                    pygame.mixer.music.load(
-                            'lobg.mp3'
-                                            )
-                    pygame.mixer.music.play(
-                            )
+                    pygame.mixer.music.load('lobg.mp3')
+                    pygame.mixer.music.play()
                     
-                if abs(
-                        px-R.randint(
-                                0
-                                ,
-                                600
-                                     )-10
-                        )<6 and abs(
-                        py-R.randint(
-                                0
-                                ,
-                                300
-                                     )
-                                                         )<6:
+                if abs(px-R.randint(0,600)-10)<6 and abs(
+                        py-R.randint(0,300))<6:
                     px=px+10
-                    screen.fill(
-                            w
-                            )
+                    screen.fill(w)
                     game_over=True
-                    pygame.mixer.music.load(
-                            'lobg.mp3'
-                                            )
-                    pygame.mixer.music.play(
-                            ) 
-        pygame.display.update(
-                )
+                    pygame.mixer.music.load('lobg.mp3')
+                    pygame.mixer.music.play() 
+        pygame.display.update()
         clock.tick(60)  #fps of game
 
-        end_time=t.time(
-                )
+        end_time=t.time()
         
     pygame.quit
-    quit(
-            )
+    quit()
 if name!='':
-        welcome(
-                )
+        welcome()
 '''import mysql.connector as m
 mydb=m.connect(
 host='localhost',
